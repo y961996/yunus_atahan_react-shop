cd5b6f27352bb48d7c6351edc3289143298f5257
Yunus Atahan <yunus10atahan@gmail.com>
2022-05-24 12:58:43 +0300

    reset the apollo test.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import './App.css';
-import gql from 'graphql-tag';
-import {useQuery} from 'react-apollo';
-
-const CURRENCIES = gql`
-query getCurrencies {
-  currencies {
-    label,
-    symbol
-  }
-}
-`;
-
-const Currencies = () => {
-  const {loading, error, data} = useQuery(CURRENCIES);
-
-  if(loading) return <p>Loading ...</p>;
-  if(error) return <p>Error! </p>
-
-  return data.currencies.map(({ label, symbol }) => (
-    <div key="label">
-      <p>
-        {symbol} : {label}
-      </p>
-    </div>
-  ));
-};
 
 function App() {
   return (
     <div className="App">
-      <Currencies></Currencies>
+      <h1>Hello World!</h1>
     </div>
   );
 }
