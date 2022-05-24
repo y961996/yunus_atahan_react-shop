9b902568faf91c1069d7e2b45925f10a027539c2
Yunus Atahan <yunus10atahan@gmail.com>
2022-05-24 13:48:25 +0300

    react with apollo works.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_CATEGORY_NAMES = gql `
+  query GetCategoryNames {
+    categories {
+      name
+    }
+  }
+`
+
+function GetCategoryNames() {
+  const { loading, error, data } = useQuery(GET_CATEGORY_NAMES);
+  
+  if(loading) return <p> Loading ... </p>
+  if(error) return <p> Error : {error}</p>
+
+  return data.categories.map(category => (
+    <div key={category.name}>
+      <p>{category.name}</p>
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <GetCategoryNames></GetCategoryNames>
     </div>
   );
 }
