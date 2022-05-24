7dd77e7a48a9c32dea6ecf15e0144dc4cb3a49b0
Yunus Atahan <yunus10atahan@gmail.com>
2022-05-24 13:03:22 +0300

    hard reset again...

--- src/index.js
+++ src/index.js
@@ -3,45 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
-import {ApolloProvider} from 'react-apollo';
-import {ApolloClient} from 'apollo-client';
-import {HttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-
-const GRAPH_QL_API_URL = "http://localhost:4000/";
-const httpLink = new HttpLink({
-  uri: GRAPH_QL_API_URL
-});
-
-const cache = new InMemoryCache();
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache
-});
-
-/*
-const CURRENCIES = gql`
-query getCurrencies {
-  currencies {
-    label,
-    symbol
-  }
-}
-`;
-
-apolloClient.query({
-  query: CURRENCIES
-}).then(result => console.log(result));
-*/
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
 
