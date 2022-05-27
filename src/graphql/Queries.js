aa4cbf41200700f2b1dd69aa57184f74cbc014fe
Yunus Atahan <yunus10atahan@gmail.com>
2022-05-27 19:21:23 +0300

    add querires.

--- src/graphql/Queries.js
+++ src/graphql/Queries.js
@@ -33,3 +33,95 @@ export const getAllData = gql`
   }
 }
 `;
+
+export const getAllDataForPLP = gql`
+{
+  categories {
+    name,
+    products {
+      id,
+      name,
+      inStock,
+      gallery,
+      category,
+      prices {
+        currency {
+          label,
+          symbol
+        }
+        amount
+      }
+      brand
+    }
+  }
+}
+`;
+
+// "huarache-x-stussy-le"
+export const getProductForCartById = gql`
+{
+	product(id:$id) {
+    id,
+    name,
+    inStock,
+    gallery,
+    attributes {
+      id,
+      name,
+      type,
+      items {
+        displayValue,
+        value,
+        id
+      }
+    }
+    prices {
+      currency {
+        symbol
+      }
+      amount
+    }
+    brand
+  }
+}
+`;
+
+export const getCurrencies = gql`
+{
+	currencies {
+    label,
+    symbol
+  }
+}
+`;
+
+// "huarache-x-stussy-le"
+export const getProductForDisplayById = gql`
+{
+	product(id:$id) {
+    id,
+    name,
+    inStock,
+    gallery,
+    description,
+    attributes {
+      id,
+      name,
+      type,
+      items {
+        displayValue,
+        value,
+        id
+      }
+    }
+    prices {
+      currency {
+        label,
+        symbol
+      }
+      amount
+    }
+    brand
+  }
+}
+`;
