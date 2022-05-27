1f71655ed17bcd30d9c8768cd169ee969e04912d
Yunus Atahan <yunus10atahan@gmail.com>
2022-05-27 12:13:35 +0300

    Add ApolloClient and Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import CategoryPage from './pages/CategoryPage';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Hello World!</h1>
-  </React.StrictMode>
+  <ApolloProvider client={apolloClient}>
+    <React.StrictMode>
+      <CategoryPage></CategoryPage>
+    </React.StrictMode>
+  </ApolloProvider>
 );
