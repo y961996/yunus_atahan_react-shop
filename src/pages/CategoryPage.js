e8b03281bdf176375d06b4d8772758cf68c5beb9
Yunus Atahan <yunus10atahan@gmail.com>
2022-05-28 14:35:58 +0300

    Add store to category page.

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import '../styles/CategoryPage.css';
+import { connect } from 'react-redux';
+import { ADD_ITEM_TO_CART } from '../redux/ActionTypes';
 
-export default class CategoryPage extends Component {
+class CategoryPage extends Component {
   render() {
     return (
       <div>CategoryPage</div>
     )
   }
 }
+
+function mapStateToProps(state){
+  return {
+    cartItems: state.cartItems,
+  };
+};
+
+function mapDispatchToProps(dispatch){
+  return {
+    addItemToCart: () => dispatch(ADD_ITEM_TO_CART()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryPage);
